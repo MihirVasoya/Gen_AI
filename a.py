88385dad8784a1f79793792c64af6a9bea9fb68f
Mihir91 <108136662+MihirVasoya@users.noreply.github.com>
2023-08-07 17:04:25 +0530

    Update a.py

--- a.py
+++ a.py
@@ -10,7 +10,7 @@ import pytesseract
 import re
 import requests
 with st.sidebar:
-   openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+   openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not)
 # os.environ['OPENAI_API_KEY'] ="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"
 # openai.api_key = "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"
 pytesseract.pytesseract.tesseract_cmd = 'C://Program Files//Tesseract-OCR//tesseract.exe'
