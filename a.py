d473965440d27d0a3cf3eddd48480a6334d1129f
Mihir91 <108136662+MihirVasoya@users.noreply.github.com>
2023-08-07 16:02:50 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,197 +1,265 @@
 import streamlit as st
+import openai
+import os
+import fitz
 import json
 import PyPDF2
 from pdf2image import convert_from_path
-from pytesseract import image_to_string 
+from pytesseract import image_to_string
 import pytesseract
-import openai
-import fitz
+import re
+import requests
 
 
-openai.api_key = "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"
+api_key = os.getenv("OPENAI_API_KEY")
+st.write("API Key:", api_key)
 pytesseract.pytesseract.tesseract_cmd = 'C://Program Files//Tesseract-OCR//tesseract.exe'
 
+def get_completion(prompt, model="gpt-3.5-turbo-16k"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    return response.choices[0].message["content"]
 
-# Title and instructions
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with fitz.open(pdf_path) as pdf_document:
+        for page_num in range(pdf_document.page_count):
+            page = pdf_document.load_page(page_num)
+            text += page.get_text()
+    return text
+
+def chat_interface(extracted_text):
+    # st.write("You can ask your questions here:")
+    user_input=("Give me the List of drugs according to case wise?",
+                "Which demonstrate the causality between the drug and the side effect according to case wise ?",
+                "what is the side effect the patient experienced? Which drug was responsible for it?"
+                )
+    prompt = f""" Mention all the answers case wise for example
+        case 1:
+            answer all three questions
+        case 2:
+            answer all three questions
+         que:```{user_input}```
+         data: '''{extracted_text}'''
+         """
+    response = get_completion(prompt)
+    st.write(response)
+   
+
+    user_input3 = st.text_area("You can ask your questions: ")
+
+    if user_input3:
+        prompt1 = f"""
+         que:```{user_input3}```
+         data: '''{extracted_text}'''
+         """
+        response2 = get_completion(prompt1)
+        st.write("Answer", response2)
+       
+        #st.json(output_data)
+
+def main():
+   
+
+    # Sidebar with the dropdown menu
+    with st.sidebar:
+           
+        st.write("Sidebar")
+        selected_option = st.selectbox("Select an option", ["Select Option","Translate_OCR", "Dynamic_OCR", "Research Paper Summary"],index=0)
 
 
-# Sidebar with three buttons
-with st.sidebar:
-    st.write("Sidebar")
-    selected_option = st.selectbox("Select an option", ["Language_Convertor", "Pdf_Convertor", "Research Paper"])
+    if selected_option == 'Select Option':
+        st.title("GenrativeAI")
+        st.write("Welcome")
+        st.write(("👈Pick an option for left"))
+
+    elif selected_option == "Research Paper Summary":
+        st.title(" IQVIA DEMO")
+        # st.write("You selected Research Paper.")
+        # st.write("Please upload a Research Paper (PDF).")
+        uploaded_file = st.file_uploader("Upload a Document", type=["pdf"])
+
+        if uploaded_file is not None:
+            st.write("Document Uploaded Successfully!")
+
+            # Extract text from the uploaded PDF
+            extracted_text = extract_text_from_pdf(uploaded_file.name)
+
+            # Display the file details (optional)
+            st.write("File name:", uploaded_file.name)
+            st.write("File size:", uploaded_file.size, "bytes")
+
+            # Chat interface
+            chat_interface(extracted_text)  # Pass the extracted text to the chat interface
 
 
-# File uploader widget
 
-# Display content based on button clicks
+    elif selected_option == "Translate_OCR":
+       
+        #st.write("You selected Research Paper.")
+            st.title(" IQVIA DEMO")
+            # st.write("Please upload a Language Paper (PDF).")
+            uploaded_file = st.file_uploader("Upload a Document", type=["pdf"])
 
+            if uploaded_file is not None:
+                st.write("Document Uploaded Successfully!")
 
-# Check if a file was uploaded
-# if uploaded_file is not None:
-#     # Display some information about the uploaded file
-#     st.write("File name:", uploaded_file.name)
-#     st.write("File size:", uploaded_file.size, "bytes")
+                # Extract text from the uploaded PDF
+               
+                # Display the file details (optional)
+                st.write("File name:", uploaded_file.name)
+                st.write("File size:", uploaded_file.size, "bytes")
 
-if selected_option=="Language_Convertor":
-    uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
-    if uploaded_file is not None:
-    # Display some information about the uploaded file
-        st.write("File name:", uploaded_file.name)
-        st.write("File size:", uploaded_file.size, "bytes")
+                # Chat interface
+                # chat_interface(extracted_text)
 
-        def get_completion(prompt, model="gpt-3.5-turbo-16k"):
-            messages = [{"role": "user", "content": prompt}]
-            response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-                
-            temperature=0,
-            #      temperature=0, # this is the degree of randomness of the model's output
-            )
-            return response.choices[0].message["content"]
-        
-        pdfFileObject = open(uploaded_file.name, 'rb')
+               
 # creating a pdf reader object
-        pdfReader = PyPDF2.PdfReader(pdfFileObject)
-        text=[]
 
-        for i in range(0,len(pdfReader.pages)):
-        # creating a page object
-            pageObj = pdfReader.pages[i].extract_text()
-            pageObj= pageObj.replace('\t\r','')
-            pageObj= pageObj.replace('\xa0','')
-            # extracting text from page
-            text.append(pageObj)
-                
-        # for i in range(len(text)):
-        prompt =f"""UserinputData in japanese converted to english "
+                def get_completion(prompt, model="gpt-3.5-turbo-16k"):
+                    messages = [{"role": "user", "content": prompt}]
+                    response = openai.ChatCompletion.create(
+                    model=model,
+                    messages=messages,
+                       
+                    temperature=0.7,
+                    #      temperature=0, # this is the degree of randomness of the model's output
+                    )
+                    return response.choices[0].message["content"]
+               
+                pdfFileObject = open(uploaded_file.name, 'rb')
+                pdfReader = PyPDF2.PdfReader(pdfFileObject)
+                text=[]
+                summary=' '
+                for i in range(0,len(pdfReader.pages)):
+               
+                    pageObj = pdfReader.pages[i].extract_text()
+                    pageObj= pageObj.replace('\t\r','')
+                    pageObj= pageObj.replace('\xa0','')
+                   
+                    text.append(pageObj)
+                # for i in range(len(text)):
+                prompt =f"""UserinputData in japanese converted to english "
 
-        ```{text}```
-            """
-        try:
-                response = get_completion(prompt)
-        except:
-                response = get_completion(prompt)
-        prompt1 =f"""
-        Your task is to convert the data into json format. 
-        Check if the details are related to each other for example someone's details,
-        which can inlcude name, date of birth, age, gender, weight, height etc then those should be included as a sub dict.' 
-        Entire json format should be editable as this is a pdf editable form.
+                {text}"""
+                try:
+                    response = get_completion(prompt)
+                except:
+                    response = get_completion(prompt)
 
-        ```{response}``
-        """
-        response1 = get_completion(prompt1)
-        json_data = json.loads(response1)
+                prompt1 =f"""
+                Your task is to convert the data into json format
+            Format the json proper key value pair.
+            Check if the details are related to each other for example someone's details,
+                            if the data contains any boxes, do create the proper check boxes for someone to tick or untick it.'
+            which can inlcude name, date of birth, age, gender, weight, height etc then those should be included as a sub dict
+            Entire json format should be editable as this is a pdf editable form
 
-        st.write("Output:")
-        st.json(json_data)
-        
+                ```{response}``
+                """
+                response1 = get_completion(prompt1)
+
+                json_data1 = json.loads(response1)
+       
+                # with open("response2.json", "w") as f:
+                st.write("Output:")
+                st.json(json_data1)
+                    #st.write(response)
+                    #summary= summary+' ' +json_data1 +'\n\n'
+
+                # Function to ask questions and get answers from the chatbot API
+               
+
+                # data = get_text_from_any_pdf(uploaded_file.name)
+                # st.write(data)
+                # input_text = f"""
+                #         UserInput Data is spanish or japanese convert into english
+                       
+                #         Context: {data}
+                   
+
+                #     Answer:
+                #     """
+
+                # response = get_completion(input_text)
+                # st.write(response)
 
 
-        # Function to ask questions and get answers from the chatbot API
-        
-        
+    else:
+        st.title(" IQVIA DEMO")
+        # st.write("Please upload a CIOMS Paper (PDF).")
+        uploaded_file = st.file_uploader("Upload a Document", type=["pdf"])
 
+        if uploaded_file is not None:
+            st.write("Document Uploaded Successfully!")
 
-        
        
 
-if selected_option=="Pdf_Convertor":
-    uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
-    if uploaded_file is not None:
-        st.write("File name:", uploaded_file.name)
-        st.write("File size:", uploaded_file.size, "bytes")
-    # Display some information about the uploaded file
-        def convert_pdf_to_img(pdf_file):
-          return convert_from_path(pdf_file, poppler_path=r'C://Users//Laptop//Downloads//Release-23.07.0-0 (2)//poppler-23.07.0//Library//bin')
+            st.write("File name:", uploaded_file.name)
+            st.write("File size:", uploaded_file.size, "bytes")
+
+            # Chat interface
+            # Display the file details (optional)
+            # chat_interface(extracted_text)
+
+            def convert_pdf_to_img(pdf_file):
+                return convert_from_path(pdf_file, poppler_path=r'C://Program Files//poppler-23.07.0//Library//bin')
 
 
-        def convert_image_to_text(file):
-        
-            
-            text = image_to_string(file)
-            return text
+            def convert_image_to_text(file):  
+                text = image_to_string(file)
+                return text
 
 
-        def get_text_from_any_pdf(pdf_file):
-        
-            images = convert_pdf_to_img(pdf_file)
-            final_text = ""
-            for pg, img in enumerate(images):
-                
-                final_text += convert_image_to_text(img)
-                #print("Page n°{}".format(pg))
-                #print(convert_image_to_text(img))
-            
-            return final_text
+            def get_text_from_any_pdf(pdf_file):
+               
+                images = convert_pdf_to_img(pdf_file)
+                final_text = ""
+                for pg, img in enumerate(images):
+                   
+                    final_text += convert_image_to_text(img)
+                    #print("Page n°{}".format(pg))
+                    #print(convert_image_to_text(img))
+               
+                return final_text
 
 
 
-        def get_completion(prompt, model="gpt-3.5-turbo-16k"):
-            messages = [{"role": "user", "content": prompt}]
-            response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            temperature=0, # this is the degree of randomness of the model's output
-        )
-            return response
-
-        ext_text = get_text_from_any_pdf(uploaded_file.name) 
-
-
-        input_text = f""" Your task is to convert the data into json format. 
-        Check if the details are related to each other for example someone's details,
-                        if the data contains any boxes, do create the proper check boxes for someone to tick or untick it.' 
-        which can inlcude name, date of birth, age, gender, weight, height etc then those should be included as a sub dict
-        Entire json format should be editable as this is a pdf editable form.
-                
-                
-                Context: {ext_text}
-            
-
-            
-            """
-        response = get_completion(input_text)
-        with open("response2.json", "w") as f:
-            f.write(response)
-if selected_option=="Research Paper":
-    uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
-    if uploaded_file is not None:
-        def get_completion(prompt, model="gpt-3.5-turbo-16k"):
-            messages = [{"role": "user", "content": prompt}]
-            response = openai.ChatCompletion.create(
+            def get_completion(prompt, model="gpt-3.5-turbo-16k"):
+                messages = [{"role": "user", "content": prompt}]
+                response = openai.ChatCompletion.create(
                 model=model,
                 messages=messages,
-                temperature=0.7,
-            )
-            return response
+                temperature=0, # this is the degree of randomness of the model's output
+                )
+                return response.choices[0].message["content"]
 
-        def extract_text_from_pdf(pdf_path):
-            text = ""
-            with fitz.open(pdf_path) as pdf_document:
-                for page_num in range(pdf_document.page_count):
-                    page = pdf_document.load_page(page_num)
-                    text += page.get_text()
-            return text
-        extracted_text = extract_text_from_pdf(uploaded_file.name)
+            ext_text = get_text_from_any_pdf(uploaded_file.name)
 
-        
-        
-        
-                
-    
- # st.write("You can ask your questions here:")
-        user_input=("1- list of drugs prescribed to the patient"
-                    "2- What side effects were experienced by the patient after he started taking the drugs ? What was the diagnosis",
-                    "3- Which drug was responsible for this side effect? Which sentences demonstrate the causality between the drug and the side effect?",
-                    "4- Is Peripheral Neuropathy a listed / known side effect?"
-        )
-        prompt = f""" Mention all the  question and answer the question wise first question then give them answert
-        
 
-        que:```{user_input}``` 
-        data: '''{extracted_text}'''
-        """
-        response = get_completion(prompt)
-        st.write(response)
-        # st.write(response.choices[0].message['content'])
+            input_text = f""" Your task is to convert the data into json format
+            Format the json proper key value pair.
+            Check if the details are related to each other for example someone's details,
+             if the data contains any checkboxes if it is tick or untick then true and false Should come,.'
+            which can inlcude name, date of birth, age, gender, weight, height etc then those should be included as a sub dict
+            Entire json format should be editable as this is a pdf editable form.
+                   
+                   
+                    Context: {ext_text}
+               
+
+               
+                """
+            response = get_completion(input_text)
+            json_data = json.loads(response)
+           
+            # with open("response2.json", "w") as f:
+            st.write("Output:")
+            st.json(json_data)
+
+if __name__ == "__main__":
+    main()
