0662d1034cc0351bbd3173f2fe1bb8112bbe13f6
Mihir91 <108136662+MihirVasoya@users.noreply.github.com>
2023-08-07 16:25:05 +0530

    Update a.py

--- a.py
+++ a.py
@@ -12,7 +12,7 @@ import requests
 
 
 
-openai.api_key = ""
+openai.api_key = "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"
 pytesseract.pytesseract.tesseract_cmd = 'C://Program Files//Tesseract-OCR//tesseract.exe'
 
 def get_completion(prompt, model="gpt-3.5-turbo-16k"):
